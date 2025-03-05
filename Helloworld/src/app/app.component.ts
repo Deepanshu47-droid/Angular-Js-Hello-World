aefa4f4c36569fce15915004da8309a13cf596ce
Deepanshu malviya <deepanshumalviya58@gmail.com>
2025-03-05 22:02:16 +0530

    [Deepanshu malviya]- Add. To display \“Hello from BridgeLabz\” using One Way Data Bindings

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -10,4 +10,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Helloworld';
+
+  ngOnInit(): void {
+    this.title = "Hello from BridgeLabz.";
+  }
 }
